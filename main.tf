53e2abd00465173e2e0d716cf67f1f30b562e5c8
vikrammehta541 <60758475+vikrammehta541@users.noreply.github.com>
2025-06-18 16:46:03 -0500

    update

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket         = "vhm-terraform-state-bucket"
+    key            = "Projects\tf-gitaction-codepipeline-project/terraform.tfstate"
+    dynamodb_table = "vhm-db-terraform-locks"
+    region         = "us-east-1"
+  }
 }
 
 resource "aws_instance" "web" {
