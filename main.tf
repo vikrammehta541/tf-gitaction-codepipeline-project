233bc895385ba184813eb2ebdfbc536e061c3c37
vikrammehta541 <60758475+vikrammehta541@users.noreply.github.com>
2025-06-18 16:49:21 -0500

    update

--- main.tf
+++ main.tf
@@ -1,19 +1,17 @@
+# Terraform Settings Block
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      #version = "~> 5.0" # Optional but recommended in production
     }
   }
+}
 
-  required_version = ">= 1.0"
-
-  backend "s3" {
-    bucket         = "vhm-terraform-state-bucket"
-    key            = "Projects\tf-gitaction-codepipeline-project/terraform.tfstate"
-    dynamodb_table = "vhm-db-terraform-locks"
-    region         = "us-east-1"
-  }
+# Provider Block
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = "us-east-1"
 }
 
 resource "aws_instance" "web" {
