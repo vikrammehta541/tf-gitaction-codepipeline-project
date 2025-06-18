a8c6739659ba01131022229c423326055a5f1597
vikrammehta541 <60758475+vikrammehta541@users.noreply.github.com>
2025-06-18 15:52:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 (change as needed)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
 
