2dcaf0560d443b9bd04567d955b9dadb962e097a
Mehta <METHAVI1@Alcon.net>
2025-06-19 10:55:21 -0500

    update

--- main.tf
+++ main.tf
@@ -9,3 +9,12 @@ resource "aws_instance" "web" {
   }
 }
 
+# create a ec2 instance
+resource "aws_instance" "web1" {
+  ami           = "ami-0742b4e673072066f"  # Amazon Linux 2 (us-east-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-EC2-1"
+  }
+}
