a3c3f855aa9fe118274de9dd31abb2229bdae719
Mehta <METHAVI1@Alcon.net>
2025-06-19 11:02:59 -0500

    new

--- main.tf
+++ main.tf
@@ -18,3 +18,14 @@ resource "aws_instance" "beb1" {
     Name = "EC2-102"
   }
 }
+
+
+# create a ec2 instance
+resource "aws_instance" "Alb1" {
+  ami           = "ami-0742b4e673072066f"  # Amazon Linux 2 (us-east-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "EC2-103"
+  }
+}
